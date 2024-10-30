08906ad63ac69367457e6bf260817ed082e3adce
kopher <kopher1601@gmail.com>
2024-10-30 20:03:18 +0900

    kopherlog feat: project init .

---
+++ kopherlog/src/main/kotlin/jp/co/kopher1601/kopherlog/KopherlogApplication.kt
@@ -0,0 +1,11 @@
+package jp.co.kopher1601.kopherlog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopherlogApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopherlogApplication>(*args)
+}
---
+++ kopherlog/src/test/kotlin/jp/co/kopher1601/kopherlog/KopherlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package jp.co.kopher1601.kopherlog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopherlogApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
