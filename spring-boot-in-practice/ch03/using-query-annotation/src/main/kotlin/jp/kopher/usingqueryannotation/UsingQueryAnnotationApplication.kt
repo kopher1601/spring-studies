c6af9c1173045395c0904b80e59eb2c09b0816b8
kopher <kopher1601@gmail.com>
2024-12-19 00:01:10 +0900

    spring boot in practice : ch03 - @Query

---
+++ spring-boot-in-practice/ch03/using-query-annotation/src/main/kotlin/jp/kopher/usingqueryannotation/UsingQueryAnnotationApplication.kt
@@ -0,0 +1,11 @@
+package jp.kopher.usingqueryannotation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UsingQueryAnnotationApplication
+
+fun main(args: Array<String>) {
+    runApplication<UsingQueryAnnotationApplication>(*args)
+}
---
+++ spring-boot-in-practice/ch03/using-query-annotation/src/test/kotlin/jp/kopher/usingqueryannotation/UsingQueryAnnotationApplicationTests.kt
@@ -0,0 +1,13 @@
+package jp.kopher.usingqueryannotation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UsingQueryAnnotationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
