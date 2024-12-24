cd7ec30943cc3c4f6d957f51861e324459370a18
kopher <kopher1601@gmail.com>
2024-12-25 08:07:06 +0900

    spring boot in practice : ch04 - Study health endpoint

--- spring-boot-in-practice/ch04/spring-boot-actuator/build.gradle.kts
+++ spring-boot-in-practice/ch04/spring-boot-actuator/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    runtimeOnly("com.h2database:h2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
