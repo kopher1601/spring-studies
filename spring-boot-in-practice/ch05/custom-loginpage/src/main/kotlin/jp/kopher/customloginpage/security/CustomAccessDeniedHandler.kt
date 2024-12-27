3ecae16b77141e68bfbadca1c57384854d033ea3
kopher <kopher1601@gmail.com>
2024-12-28 00:45:54 +0900

    spring boot in practice : ch05 - InMemoryUserDetailManager and PasswordEncoder

---
+++ spring-boot-in-practice/ch05/custom-loginpage/src/main/kotlin/jp/kopher/customloginpage/security/CustomAccessDeniedHandler.kt
@@ -0,0 +1,19 @@
+package jp.kopher.customloginpage.security
+
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.access.AccessDeniedException
+import org.springframework.security.web.access.AccessDeniedHandler
+import org.springframework.stereotype.Component
+
+@Component
+class CustomAccessDeniedHandler: AccessDeniedHandler {
+    override fun handle(
+        request: HttpServletRequest?,
+        response: HttpServletResponse?,
+        accessDeniedException: AccessDeniedException?
+    ) {
+
+        response?.sendRedirect(request?.contextPath + "/accessDenied")
+    }
+}
--- spring-boot-in-practice/ch05/custom-loginpage/src/main/kotlin/jp/kopher/customloginpage/security/SecurityConfiguration.kt
+++ spring-boot-in-practice/ch05/custom-loginpage/src/main/kotlin/jp/kopher/customloginpage/security/SecurityConfiguration.kt
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.core.userdetails.User
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.access.AccessDeniedHandler
@@ -41,6 +44,11 @@ class SecurityConfiguration(
         return WebSecurityCustomizer { web ->
             web.ignoring().requestMatchers("/webjars/**", "/images/**", "/css/**", "/h2-console/**")
         }
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder()
     }
 
 }
