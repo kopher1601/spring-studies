abb11fe10c6dfee1041a26a1f403f0ae3eff5922
kopher <kopher1601@gmail.com>
2025-03-25 16:17:14 +0900

    Project init

---
+++ kotlin-hexagonal/src/test/kotlin/jp/co/kopher/kotlinhexagonal/KotlinHexagonalApplicationTests.kt
@@ -0,0 +1,13 @@
+package jp.co.kopher.kotlinhexagonal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinHexagonalApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
