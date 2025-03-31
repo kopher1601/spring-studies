c4e552415392458b1d5c7ebd7689068a1d04a9af
kopher <kopher1601@gmail.com>
2025-03-31 09:02:51 +0900

    library-app : finish

--- library-app/src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
+++ library-app/src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
@@ -9,7 +9,7 @@ class UserLoanHistory(
     val user: User,
 
     val bookName: String,
-    var isReturn: Boolean,
+    var status: UserLoanStatus = UserLoanStatus.LOANED,
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,6 +17,6 @@ class UserLoanHistory(
 ) {
 
     fun doReturn() {
-        this.isReturn = true
+        this.status = UserLoanStatus.RETURNED
     }
 }
--- library-app/src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ library-app/src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -4,6 +4,7 @@ import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.dto.user.request.UserCreateRequest
 import com.group.libraryapp.dto.user.request.UserUpdateRequest
+import com.group.libraryapp.dto.user.response.UserLoanHistoryResponse
 import com.group.libraryapp.dto.user.response.UserResponse
 import com.group.libraryapp.util.fail
 import com.group.libraryapp.util.findByIdOrThrow
