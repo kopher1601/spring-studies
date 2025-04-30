5a71cbfd3b8f5f3d6791c6e42c9a19b58a6bf1fd
kopher <kopher1601@gmail.com>
2025-05-01 00:26:30 +0900

    Setup project Spring Transaction with Kotlin

---
+++ kotlin-springtx/src/main/kotlin/jp/co/kopher/kotlinspringtx/KotlinSpringtxApplication.kt
@@ -0,0 +1,11 @@
+package jp.co.kopher.kotlinspringtx
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringtxApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringtxApplication>(*args)
+}
