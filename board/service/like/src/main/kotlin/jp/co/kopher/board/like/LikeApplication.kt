b369eef00526d250ff12c2409dcabddae2bfc8f5
kopher <kopher1601@gmail.com>
2025-05-01 00:04:41 +0900

    Project setup

---
+++ board/service/like/src/main/kotlin/jp/co/kopher/board/like/LikeApplication.kt
@@ -0,0 +1,12 @@
+package jp.co.kopher.board.like
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LikeApplication {
+}
+
+fun main(args: Array<String>) {
+    runApplication<LikeApplication>(*args)
+}
