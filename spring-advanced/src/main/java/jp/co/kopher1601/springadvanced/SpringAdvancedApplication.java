c959f3e45df694b392053ab49d4090d1bab709d1
kopher <kopher1601@gmail.com>
2025-05-16 02:43:23 +0900

    Order V0

---
+++ spring-advanced/src/main/java/jp/co/kopher1601/springadvanced/SpringAdvancedApplication.java
@@ -0,0 +1,13 @@
+package jp.co.kopher1601.springadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdvancedApplication.class, args);
+    }
+
+}
---
+++ spring-advanced/src/test/java/jp/co/kopher1601/springadvanced/SpringAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.kopher1601.springadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
