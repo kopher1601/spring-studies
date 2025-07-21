749ad10f6251bd246b173deb9e8a3bf314ef6064
kopher <kopher1601@gmail.com>
2025-07-21 22:00:09 +0900

    init

---
+++ spring-board/src/main/java/jp/kopher1601/springboard/SpringBoardApplication.java
@@ -0,0 +1,13 @@
+package jp.kopher1601.springboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoardApplication.class, args);
+    }
+
+}
