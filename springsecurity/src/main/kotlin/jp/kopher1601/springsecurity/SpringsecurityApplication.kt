e046469a47f3cfd63efb128dc210997105c41c69
kopher <kopher1601@gmail.com>
2025-08-07 07:47:08 +0900

    init

---
+++ springsecurity/src/main/kotlin/jp/kopher1601/springsecurity/SpringsecurityApplication.kt
@@ -0,0 +1,11 @@
+package jp.kopher1601.springsecurity
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringsecurityApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringsecurityApplication>(*args)
+}
