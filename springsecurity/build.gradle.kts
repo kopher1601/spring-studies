d2597347d8de7364919eb4818937842c889acc24
kopher <kopher1601@gmail.com>
2025-08-08 19:18:27 +0900

    Add spring security dependency

--- springsecurity/build.gradle.kts
+++ springsecurity/build.gradle.kts
@@ -22,6 +22,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.security:spring-security-test")
+
+    // security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
