3432bc8e9cf5d7894b24efad07f50cd53f840d3e
kopher <kopher1601@gmail.com>
2025-08-08 23:54:51 +0900

    Update ProjectSecurityConfig to disable formLogin & httpBasic

--- springsecurity/src/main/kotlin/jp/kopher1601/springsecurity/config/ProjectSecurityConfig.kt
+++ springsecurity/src/main/kotlin/jp/kopher1601/springsecurity/config/ProjectSecurityConfig.kt
@@ -2,7 +2,6 @@ package jp.kopher1601.springsecurity.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.SecurityFilterChain
 
@@ -11,12 +10,13 @@ class ProjectSecurityConfig {
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http.formLogin{ form -> form.disable()}
+        http.httpBasic{ basic -> basic.disable()}
         http.authorizeHttpRequests { requests -> requests
             .requestMatchers("/myAccount","/myBalance","/myCards","/myLoans").authenticated()
             .requestMatchers("/notices", "/contact", "/error").permitAll()
         }
-        http.formLogin(withDefaults())
-        http.httpBasic(withDefaults())
+
         return http.build()
     }
 }
