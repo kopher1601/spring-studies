b8bcdc373765de45bc60bf7070c87a7a255554b5
kopher <kopher1601@gmail.com>
2025-08-20 09:19:28 +0900

    Environments

--- springsecurity/src/main/kotlin/jp/kopher1601/springsecurity/config/ProjectSecurityProdConfig.kt
+++ springsecurity/src/main/kotlin/jp/kopher1601/springsecurity/config/ProjectSecurityProdConfig.kt
@@ -11,9 +11,9 @@ import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker
 
-@Profile("!prod")
+@Profile("prod")
 @Configuration
-class ProjectSecurityConfig {
+class ProjectSecurityProdConfig {
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
