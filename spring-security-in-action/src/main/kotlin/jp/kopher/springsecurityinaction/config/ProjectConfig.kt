6f2b6c3dcdd351c097f87145d4941506ebdaf5bf
kopher <kopher1601@gmail.com>
2025-10-26 14:46:19 +0900

    ssia-ch2-ex5

--- spring-security-in-action/src/main/kotlin/jp/kopher/springsecurityinaction/config/ProjectConfig.kt
+++ spring-security-in-action/src/main/kotlin/jp/kopher/springsecurityinaction/config/ProjectConfig.kt
@@ -2,38 +2,22 @@ package jp.kopher.springsecurityinaction.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.crypto.password.NoOpPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
-class ProjectConfig {
-    @Bean
-    fun userDetailsService(): UserDetailsService {
-        return InMemoryUserDetailsManager(
-            User.withUsername("john")
-                .password("12345")
-                .authorities("read")
-                .build()
-        )
-    }
-
-    @Bean
-    fun passwordEncoder(): PasswordEncoder {
-        return NoOpPasswordEncoder.getInstance()
-    }
-
+class ProjectConfig(
+    private val authenticationProvider: AuthenticationProvider,
+) {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http.httpBasic(Customizer.withDefaults())
         http.authorizeHttpRequests { auth ->
             auth.anyRequest().authenticated()
         }
+        http.authenticationProvider(authenticationProvider)
         return http.build()
     }
 }
