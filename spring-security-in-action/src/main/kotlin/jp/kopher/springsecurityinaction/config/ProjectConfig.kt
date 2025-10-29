b76e032b324aaade9fd8a9872b8475c3a8760832
kopher <kopher1601@gmail.com>
2025-10-29 22:08:02 +0900

    ssia-ch5-ex2: SecurityContext propagation and async configuration

--- spring-security-in-action/src/main/kotlin/jp/kopher/springsecurityinaction/config/ProjectConfig.kt
+++ spring-security-in-action/src/main/kotlin/jp/kopher/springsecurityinaction/config/ProjectConfig.kt
@@ -1,13 +1,18 @@
 package jp.kopher.springsecurityinaction.config
 
+import org.springframework.beans.factory.InitializingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.password.NoOpPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.provisioning.JdbcUserDetailsManager
 import javax.sql.DataSource
 
+
+@EnableAsync
 @Configuration
 class ProjectConfig {
 
@@ -19,5 +24,12 @@ class ProjectConfig {
     @Bean
     fun passwordEncoder(): PasswordEncoder {
         return NoOpPasswordEncoder.getInstance()
+    }
+
+    @Bean
+    fun initializingBean(): InitializingBean {
+        return InitializingBean {
+            SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL)
+        }
     }
 }
--- spring-security-in-action/src/main/kotlin/jp/kopher/springsecurityinaction/controllers/HelloController.kt
+++ spring-security-in-action/src/main/kotlin/jp/kopher/springsecurityinaction/controllers/HelloController.kt
@@ -1,13 +1,44 @@
 package jp.kopher.springsecurityinaction.controllers
 
+import org.springframework.scheduling.annotation.Async
+import org.springframework.security.concurrent.DelegatingSecurityContextCallable
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
+
 
 @RestController
 class HelloController {
 
     @GetMapping("/hello")
-    fun hello(): String {
-        return "Hello!"
+    fun hello(authentication: Authentication): String {
+        return "Hello, ${authentication?.name}!"
+    }
+
+    @GetMapping("/bye")
+    @Async
+    fun goodbye() {
+        val context = SecurityContextHolder.getContext()
+        val authentication = context.authentication
+        println("goodbye, ${authentication?.name}") // null
+    }
+
+    @GetMapping("/ciao")
+    fun ciao(): String {
+        val task = Callable {
+            val context = SecurityContextHolder.getContext()
+            context.authentication.name
+        }
+
+        val e = Executors.newCachedThreadPool()
+        try {
+            val contextTask = DelegatingSecurityContextCallable(task)
+            return "Ciao, " + e.submit(contextTask).get() + "!"
+        } finally {
+            e.shutdown()
+        }
     }
 }
