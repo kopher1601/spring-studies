b43b65c35eaf1bdd9b2a060e128bc4c2e168038d
kopher <kopher1601@gmail.com>
2025-11-10 01:23:37 +0900

    ssia-ch16-ex2: PreAuthorize with parameter

--- spring-security-in-action/src/main/kotlin/jp/kopher/springsecurityinaction/controllers/HelloController.kt
+++ spring-security-in-action/src/main/kotlin/jp/kopher/springsecurityinaction/controllers/HelloController.kt
@@ -2,6 +2,7 @@ package jp.kopher.springsecurityinaction.controllers
 
 import jp.kopher.springsecurityinaction.services.NameService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -12,5 +13,10 @@ class HelloController(
     @GetMapping("/hello")
     fun hello(): String {
         return "Hello, ${nameService.getName()}!"
+    }
+
+    @GetMapping("/secret/names/{name}")
+    fun names(@PathVariable name: String): List<String> {
+        return nameService.getSecretNames(name)
     }
 }
--- spring-security-in-action/src/main/kotlin/jp/kopher/springsecurityinaction/services/NameService.kt
+++ spring-security-in-action/src/main/kotlin/jp/kopher/springsecurityinaction/services/NameService.kt
@@ -6,9 +6,20 @@ import org.springframework.stereotype.Service
 @Service
 class NameService {
 
+    private val secretNames = mapOf(
+        "natalie" to listOf("Energico", "Perfecto"),
+        "emma" to listOf("Fantastico")
+    )
+
+
     @PreAuthorize("hasAuthority('write')")
     fun getName(): String {
         return "Fantastico"
+    }
+
+    @PreAuthorize("#name == authentication.principal.username")
+    fun getSecretNames(name: String): List<String> {
+        return secretNames[name] ?: emptyList()
     }
 
 }
