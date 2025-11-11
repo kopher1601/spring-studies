212b5c6417b38c12d9d54e6a5fdceea965efd19c
kopher <kopher1601@gmail.com>
2025-11-11 14:18:04 +0900

    ssia-ch20-ex1: Test in Spring Security

--- spring-security-in-action/src/main/kotlin/jp/kopher/springsecurityinaction/model/Product.kt
+++
@@ -1,6 +0,0 @@
-package jp.kopher.springsecurityinaction.model
-
-class Product(
-    val name: String,
-    val owner: String,
-)
--- spring-security-in-action/src/test/kotlin/jp/kopher/springsecurityinaction/SpringSecurityInActionApplicationTests.kt
+++ spring-security-in-action/src/test/kotlin/jp/kopher/springsecurityinaction/SpringSecurityInActionApplicationTests.kt
@@ -1,13 +1,37 @@
 package jp.kopher.springsecurityinaction
 
 import org.junit.jupiter.api.Test
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.test.context.TestConstructor
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
-class SpringSecurityInActionApplicationTests {
+@AutoConfigureMockMvc
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+class SpringSecurityInActionApplicationTests(
+    private val mockMvc: MockMvc,
+) {
 
     @Test
     fun contextLoads() {
     }
 
+    @Test
+    fun helloUnauthenticated() {
+        mockMvc.perform(get("/hello"))
+            .andExpect(status().isUnauthorized)
+    }
+
+    @Test
+    @WithMockUser
+    fun `helloAuthenticated`() {
+        mockMvc.perform(get("/hello"))
+            .andExpect(content().string("Hello World!"))
+            .andExpect(status().isOk)
+    }
 }
