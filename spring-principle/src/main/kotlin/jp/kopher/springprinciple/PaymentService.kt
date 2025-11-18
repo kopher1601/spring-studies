ed0ec6957fc4b8dfbeba30e4e49a55a7c1274a1b
kopher <kopher1601@gmail.com>
2025-11-19 08:02:49 +0900

    PaymentService 2

--- spring-principle/src/main/kotlin/jp/kopher/springprinciple/PaymentService.kt
+++ spring-principle/src/main/kotlin/jp/kopher/springprinciple/PaymentService.kt
@@ -1,13 +1,28 @@
 package jp.kopher.springprinciple
 
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
 class PaymentService {
-    fun prepare(): Payment {
-        return Payment()
+    fun prepare(
+        orderId: Long,
+        currency: String,
+        foreignCurrencyAmount: BigDecimal,
+    ): Payment {
+
+        return Payment(
+            orderId = orderId,
+            currency = currency,
+            foreignCurrencyAmount = foreignCurrencyAmount,
+            exRate = BigDecimal.ZERO,
+            convertedAmount = BigDecimal.ZERO,
+            validUntil = LocalDateTime.now(),
+        )
     }
 }
 
 fun main() {
     val paymentService = PaymentService()
-    val payment = paymentService.prepare()
+    val payment = paymentService.prepare(100, "USD", BigDecimal.valueOf(50.7) )
     println("paymentService = $payment")
 }
