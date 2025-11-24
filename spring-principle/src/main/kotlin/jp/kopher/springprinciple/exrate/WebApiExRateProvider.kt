9069655198bc587b58c58a91da7c70effbbf3054
kopher <kopher1601@gmail.com>
2025-11-24 14:46:12 +0900

    Use `URI` instead of `URL` in `WebApiExRateProvider` for cleaner code and compatibility.

--- spring-principle/src/main/kotlin/jp/kopher/springprinciple/exrate/WebApiExRateProvider.kt
+++ spring-principle/src/main/kotlin/jp/kopher/springprinciple/exrate/WebApiExRateProvider.kt
@@ -5,13 +5,14 @@ import jp.kopher.springprinciple.payment.ExRateProvider
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.math.BigDecimal
+import java.net.URI
 import java.net.URL
 import java.util.stream.Collectors
 
 class WebApiExRateProvider: ExRateProvider {
     override fun getExRate(currency: String): BigDecimal {
-        val url = URL(" https://open.er-api.com/v6/latest/${currency}")
-        val connection = url.openConnection()
+        val uri = URI(" https://open.er-api.com/v6/latest/${currency}")
+        val connection = uri.toURL().openConnection()
         val br = BufferedReader(InputStreamReader(connection.getInputStream(), "UTF-8"))
         val response = br.lines().collect(Collectors.joining())
         br.close()
