f7c51c778c256911a72284ca8e70c60de2186310
kopher <kopher1601@gmail.com>
2026-05-24 01:57:19 +0900

    paymentservice 2~3

---
+++ hellospring/src/main/java/kopher/hellospring/ExRateData.java
@@ -0,0 +1,10 @@
+package kopher.hellospring;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+public record ExRateData(
+        String result,
+        Map<String, BigDecimal> rates
+) {
+}
