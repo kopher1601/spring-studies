04b0d3bb3e5f2abc6f607556638bb798d4cd7f48
kopher <kopher1601@gmail.com>
2026-05-24 13:02:03 +0900

    Separation Of Concern

--- hellospring/src/main/java/kopher/hellospring/PaymentService.java
+++ hellospring/src/main/java/kopher/hellospring/PaymentService.java
@@ -7,14 +7,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.util.stream.Collectors;
 
 public class PaymentService {
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-        // 환율 가져오기
-        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
+        BigDecimal exRate = getExRate(currency);
+
+        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
+
+        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
+
+        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
+    }
+
+    private BigDecimal getExRate(String currency) throws IOException {
+        URL url = URI.create("https://open.er-api.com/v6/latest" + currency).toURL();
         HttpURLConnection connection = (HttpURLConnection)url.openConnection();
         BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
@@ -24,16 +34,7 @@ public class PaymentService {
         System.out.println("response = " + response);
         ObjectMapper mapper = new ObjectMapper();
         ExRateData data = mapper.readValue(response, ExRateData.class);
-        BigDecimal exRate = data.rates().get("KRW");
-        System.out.println("exRate = " + exRate);
-
-        // 금액 계산
-        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
-
-        // 유효시간 계산
-        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
-
-        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
+        return data.rates().get("KRW");
     }
 
     public static void main(String[] args) throws IOException {
